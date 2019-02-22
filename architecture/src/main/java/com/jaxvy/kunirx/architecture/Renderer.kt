c1750748cfb1e5508d388dc9dcd6fbb42a85e212
jaxvy <efe.karasabun@gmail.com>
2019-02-21 23:34:09 -0800

    dev8 - kunirx  and sample activity

---
+++ architecture/src/main/java/com/jaxvy/kunirx/architecture/Renderer.kt
@@ -0,0 +1,68 @@
+package com.jaxvy.kunirx.architecture
+
+import android.util.Log
+import io.reactivex.Observable
+import io.reactivex.Scheduler
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.disposables.Disposable
+
+class Renderer<U : UIState>(
+        private val rendererConfig: RendererConfig<U>
+) {
+    private lateinit var compositeDisposable: CompositeDisposable
+
+    /**
+     * TODO: Handle
+     * 1. Errors
+     * 2. Logging
+     */
+    fun start(uiView: UIView<U>) {
+        compositeDisposable = CompositeDisposable()
+        compositeDisposable.add(run(uiView))
+    }
+
+    private fun run(uiView: UIView<U>): Disposable {
+        return uiView.uiActionDataObservable()
+                .flatMap { uiActionData ->
+                    (rendererConfig.uiActions() as? List<UIAction<U, UIAction.UIActionData>>)
+                            ?.find { uiAction ->
+                                isUIActionDataBelongsToUIAction(uiActionData, uiAction)
+                            }?.let { uiAction ->
+                                uiAction.execute(uiActionData)
+                                        .map { mutator -> uiAction.reduce(uiView.uiState, mutator) }
+                                        .subscribeOn(rendererConfig.ioScheduler)
+                            } ?: Observable.just(uiView.uiState)
+                }
+                .observeOn(rendererConfig.mainScheduler)
+                .subscribe(
+                        { newUIState ->
+                            uiView.apply {
+                                uiState = newUIState
+                                render(newUIState)
+                            }
+                        },
+                        { error ->
+                            Log.e("Renderer error", error.message)
+                        }
+                )
+    }
+
+    fun stop() {
+        if (!compositeDisposable.isDisposed) {
+            compositeDisposable.dispose()
+        }
+    }
+
+    private fun isUIActionDataBelongsToUIAction(
+            uiActionData: UIAction.UIActionData,
+            uiAction: UIAction<*, *>
+    ): Boolean = uiActionData::class.java.declaringClass == uiAction::class.java
+}
+
+interface RendererConfig<U : UIState> {
+    val mainScheduler: Scheduler
+
+    val ioScheduler: Scheduler
+
+    fun uiActions(): List<UIAction<U, *>>
+}
---
+++ counter-app/src/main/java/me/jaxvy/kunirx/counter/CounterActivity.kt
@@ -0,0 +1,73 @@
+package me.jaxvy.kunirx.counter
+
+import android.os.Bundle
+import com.jakewharton.rxbinding3.view.clicks
+import dagger.Reusable
+import io.reactivex.Observable
+import io.reactivex.Scheduler
+import kotlinx.android.synthetic.main.activity_counter.*
+import me.jaxvy.kunirx.R
+import com.jaxvy.kunirx.architecture.RendererConfig
+import com.jaxvy.kunirx.architecture.UIAction
+import com.jaxvy.kunirx.architecture.UIState
+import com.jaxvy.kunirx.architecture.UIViewActivity
+import me.jaxvy.kunirx.di.IOScheduler
+import me.jaxvy.kunirx.di.MainScheduler
+import me.jaxvy.kunirx.di.inject
+import javax.inject.Inject
+
+data class CounterActivityUIState(
+        val counterValue: Int = 0
+) : UIState
+
+@Reusable
+class CounterActivityRendererConfig @Inject constructor(
+        private val incrementClickUIAction: IncrementClickUIAction,
+        private val decrementClickUIAction: DecrementClickUIAction
+) : RendererConfig<CounterActivityUIState> {
+
+    @field:[Inject MainScheduler]
+    override lateinit var mainScheduler: Scheduler
+
+    @field:[Inject IOScheduler]
+    override lateinit var ioScheduler: Scheduler
+
+    override fun uiActions(): List<UIAction<CounterActivityUIState, *>> {
+        return listOfNotNull(
+                incrementClickUIAction,
+                decrementClickUIAction
+        )
+    }
+}
+
+class CounterActivity : UIViewActivity<CounterActivityUIState>() {
+
+    @Inject
+    lateinit var rendererConfig: CounterActivityRendererConfig
+
+    override fun rendererConfig() = rendererConfig
+
+    override fun uiActionDataObservable(): Observable<UIAction.UIActionData> {
+        return Observable.mergeArray(
+                incrementButton
+                        .clicks()
+                        .map { IncrementClickUIAction.Data(currentValue = uiState.counterValue) },
+
+                decrementButton
+                        .clicks()
+                        .map { DecrementClickUIAction.Data(currentValue = uiState.counterValue) }
+        )
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        inject(this)
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_counter)
+
+        uiState = CounterActivityUIState()
+    }
+
+    override fun render(uiState: CounterActivityUIState) {
+        counter.text = uiState.counterValue.toString()
+    }
+}
